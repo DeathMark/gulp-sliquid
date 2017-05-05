2587be51b1317eb01620ebef6fb5eb058fce8ad5
Tuan Pham <tuanpm@skylinesoft.vn>
2017-05-05 16:55:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var path		= require('path');
+var through		= require('through2');
+var gutil		= require('gulp-util');
+var PluginError	= gutil.PluginError;
+var _			= require('lodash');
+var liquify		= require('./liquify');
+
+// consts
+const PLUGIN_NAME = 'gulp-sliquid';
+
+// plugin level function (dealing with files)
+function gulpSLiquid(locals, options) {
+
+	var settings = _.defaults(options || {}, {
+		"base": false,
+		"prefix": false,
+		"filters": {}
+	});
+
+	// creating a stream through which each file will pass
+	var stream = through.obj(function (file, enc, cb) {
+
+		if (file.isNull()) {
+			return cb(null, file);
+		}
+		if (file.isStream()) {
+			return this.emit('error', new PluginError('gulp-sliquid', 'Streaming not supported'));
+		}
+
+		global.megaliquify_file	= path.basename(file.path, '.liquid');
+
+		// Clone a fresh copy, so as not to affect others
+		var tempLocals = locals ? _.clone(locals) : {};
+
+		// Apply file specific locals
+		if (file.locals) {
+			tempLocals = _.defaults(file.locals, tempLocals);
+		}
+
+		var contents	= file.contents.toString("utf-8");
+
+		liquify(contents, tempLocals, settings.base || file.base, settings.prefix, settings.filters)
+			.then(function (result) {
+				file.contents = new Buffer(result, "utf-8");
+				this.push(file);
+				return cb();
+			}.bind(this))
+			.catch(function (err) {
+				this.emit('error', err);
+				return cb();
+			}.bind(this));
+
+	});
+
+	// returning the file stream
+	return stream;
+}
+
+gulpSLiquid.liquify = liquify;
+
+// exporting the plugin main function
+module.exports = gulpSLiquid;
---
+++ liquify.js
@@ -0,0 +1,85 @@
+var fs		= require('fs');
+var path	= require('path');
+var Promise	= require('bluebird');
+var liquid	= require('sliquid');
+
+var liquify = function (contents, locals, includeBase, prefix, filters) {
+	var template;
+	var context = liquid.newContext({
+		locals: locals,
+		filters: filters
+	});
+
+	if (!contents) {
+		contents = '';
+	}
+
+	if (typeof contents != "string") {
+		template = contents;
+	} else {
+		template = liquid.compile(contents);
+	}
+
+	return new Promise(function (resolve, reject) {
+
+		context.onInclude(function (name, callback) {
+
+			var absolute	= isAbsolute(name);
+			var ext			= path.extname(name);
+			var filePath, i;
+
+			if (!absolute) {
+				if (prefix) {
+					name = prefix + name;
+				}
+
+				if (!ext) {
+					name += ".liquid";
+				}
+
+				if (Array.isArray(includeBase)) {
+					for (i in includeBase) {
+						if (includeBase.hasOwnProperty(i)) {
+							filePath	= path.join(includeBase[i], name);
+
+							if (fs.existsSync(filePath)) {
+								break;
+							}
+						}
+					}
+				} else {
+					filePath = path.join(includeBase, name);
+				}
+			} else {
+				filePath = name;
+			}
+
+			fs.readFile(filePath, 'utf8', function (err, text) {
+
+				if (err) {
+					reject(err);
+					return callback(err);
+				}
+
+				var ast = liquid.parse(text);
+				callback(null, ast);
+			});
+		});
+
+		template(context, function (err) {
+			if (err) return reject(err);
+			resolve(context.getBuffer(), template);
+		});
+	});
+
+};
+
+function isAbsolute(p) {
+	if (path.isAbsolute) {
+		return path.isAbsolute(p);
+	}
+
+	return path.normalize(p + '/') === path.normalize(path.resolve(p) + '/');
+}
+
+module.exports = liquify();
